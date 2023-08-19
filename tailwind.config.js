79df0dbd74e4c2b8b657519d9a31e6708519e342
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-08-19 15:04:34 -0500

    Intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html", "./src/**/*.js"],
+  theme: {
+    fontFamily: {
+      Tilt: ["Tilt Prism", "sans-serif"],
+      Raleway: ["Raleway", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
